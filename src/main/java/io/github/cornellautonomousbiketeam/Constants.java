b84b28be0667c534da66ef8a4d6b2d94643295a6
Daniel Glus <danielhglus@gmail.com>
2017-07-20 14:20:49 -0400

    Refactor, remove unused class

--- src/main/java/io/github/cornellautonomousbiketeam/Constants.java
+++
@@ -1,9 +0,0 @@
-package io.github.cornellautonomousbiketeam;
-
-public abstract class Constants {
-    public static final float ORIGIN_LATITUDE = 42.44814F;
-    public static final float ORIGIN_LONGITUDE = -76.48489F;
-
-    /** The approximate radius of the Earth, in meters. */
-    public static final float EARTH_RADIUS = 6371000;
-}
--- src/main/java/io/github/cornellautonomousbiketeam/Helper.java
+++ src/main/java/io/github/cornellautonomousbiketeam/Helper.java
@@ -1,34 +1,12 @@
 package io.github.cornellautonomousbiketeam;
 
-import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
 import com.jcraft.jsch.ChannelSftp.LsEntry;
 
-import io.github.cornellautonomousbiketeam.Constants;
-
 public abstract class Helper {
-    public static Point2D.Double latLongToMeters( float latitude, float longitude ) {
-        double origin_latitude_rad = Math.toRadians( Constants.ORIGIN_LATITUDE );
-        double origin_longitude_rad = Math.toRadians(
-                Constants.ORIGIN_LONGITUDE );
-        double latitude_rad = Math.toRadians( latitude );
-        double longitude_rad = Math.toRadians( longitude );
-        double a = Math.pow( Math.sin( ( latitude_rad - origin_longitude_rad ) / 2 ), 2 ) + Math.cos( latitude_rad ) * Math.cos( origin_latitude_rad ) * Math.pow( Math.sin( ( longitude_rad - origin_longitude_rad ) / 2F ), 2 );
-        double theta = 2 * Math.atan2( Math.sqrt( a ), Math.sqrt( 1 - a ) );
-        double d = theta * Constants.EARTH_RADIUS;
-        System.out.print( String.format( " [%.1f] ", d ) );
-
-        // Compute bearing
-        double y = Math.sin( longitude_rad - origin_longitude_rad ) * Math.cos( latitude_rad );
-        double x = Math.cos( origin_latitude_rad ) * Math.sin( latitude_rad ) - Math.sin( origin_latitude_rad ) * Math.cos( latitude_rad ) * Math.cos( longitude_rad - origin_longitude_rad );
-        double bearing_rad = Math.atan2( y, x );
-        return new Point2D.Double( d * Math.sin( bearing_rad ),
-                d * Math.cos( bearing_rad ) );
-    }
-
     /**
      * Copied from https://stackoverflow.com/a/30691451/1757964
      */
